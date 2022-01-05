ab303c249d103bb27c29ade8d3f42506b9345ae1
VitorBando <vitorm.804@gmail.com>
2022-01-04 21:52:51 -0300

    Aprendi sobre coleções e erros, realizei atividades praticas com esses conteudos.

---
+++ JavaScript/Map vs Objeto/script.js
@@ -0,0 +1,14 @@
+const myMap = new Map();
+
+/*myMap.set('apple', 'fruit');
+
+myMap.get("apple");
+
+myMap.delete("apple");
+
+myMap.get("apple");
+
+console.log(myMap.set());
+console.log(myMap.get());
+console.log(myMap.delete());
+console.log(myMap.get());*/
